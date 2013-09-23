8c28860f51f93e7e3e45993e5edc675a094b3678
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2013-09-23 17:16:41 +0200

    Fix gh-pages script to always generate current

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,5 +3,5 @@ git checkout master
 make clean
 make html # files are now in build/html/
 git checkout gh-pages # build/ is not removed as it is in .gitignore
-cp -rf build/html/* .
+cp -rf build/html/* current
 rm -rf build
